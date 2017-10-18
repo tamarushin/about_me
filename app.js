f322bae16ba83a374ec3fa4d7e67b97dde0da313
tamarushin <tamarushin@gmail.com>
2017-10-18 13:51:47 -0700

    upload

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+  var paper = prompt('does tama have a degree?');
+  var paper = paper.toUpperCase();
+
+    console.log('tamas degree', paper);
+    if (paper === "yes")  {
+      alert ('That\'s right!!');
+    } else {
+      alert ('your wrong, i do!');
+  }
+
+    var hobbies = prompt('does tama have hobbies?');
+    var hobbies = hobbies.toLowerCase();
+
+      console.log('TAMAS HOBBIES', hobbies);
+
+    if (hobbies === "yes") {
+        alert ('i do');
+            } else {
+        alert ('your wrong, i do!');
+      }
+
+    var animal = prompt('is tamas favorite animal a dog?');
+
+      console.log('tamas animal', animal);
+    if (animal === "yes")  {
+        alert ('grrrrrreat!');
+          } else {
+        alert ('your wrong, it is!');
+      }
+
+      var kid = prompt('does tama have a son?');
+
+        console.log('tamas son', kid);
+      if (kid === "yes")  {
+          alert ('nope, a daughter!');
+            } else {
+          alert ('Right! I have a daughter!');
+        }
